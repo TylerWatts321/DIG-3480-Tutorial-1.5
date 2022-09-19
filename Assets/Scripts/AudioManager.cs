5e101ad2116231658a758e4ace01b33fc031ea65
Tyler <gordontwatts@gmail.com>
2022-09-19 12:58:15 -0400

    Animation commit

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,6 +11,12 @@ public class AudioManager : MonoBehaviour
 
     public AudioSource musicSource;
 
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -18,25 +24,26 @@ public class AudioManager : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
-
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.L))
